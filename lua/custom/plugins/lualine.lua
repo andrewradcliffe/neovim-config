eee52d18959ea8865fd6629cbc16a9bd39bb2a50
Andrew Radcliffe <andrew.radcliffe4540@gmail.comm>
2025-04-09 15:47:21 +0100

    change lualine to show relative path to file

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -8,7 +8,12 @@ return {
 			sections = {
 				lualine_a = {'mode'},
 				lualine_b = {'branch', 'diff', 'diagnostics'},
-				lualine_c = {'filename'},
+				lualine_c = {
+					{
+						'filename',
+						path = 1,
+					},
+				},
 				lualine_x = {'lsp_status', 'filetype'},
 				lualine_y = {'progress'},
 				lualine_z = {'location'}
