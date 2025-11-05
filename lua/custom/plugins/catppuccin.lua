5bd59a103353c21df8c80378a5879f1090d4bd02
Andrew Radcliffe <andrew.radcliffe4540@gmail.com>
2025-11-05 14:17:11 +0000

    change theme to kanagawa dragon

--- lua/custom/plugins/catppuccin.lua
+++ lua/custom/plugins/catppuccin.lua
@@ -2,7 +2,7 @@
 return {
     "catppuccin/nvim",
     commit = "cb5665990a797b102715188e73c44c3931b3b42e",
-    -- enabled = false,
+    enabled = false,
     lazy = false,
     priority = 1000,
     name = "catppuccin",
--- lua/custom/plugins/kanagawa.lua
+++ lua/custom/plugins/kanagawa.lua
@@ -4,6 +4,7 @@ return {
     priority = 1000,
     name = "kanagawa",
     opts = {
+        compile = false,
         theme = "dragon",
         background = {
             dark = "dragon",
@@ -17,29 +18,32 @@ return {
                 },
             },
         },
+        overrides = function(colors)
+            local theme = colors.theme
+            return {
+                LineNrAbove = { fg = theme.ui.nontext },
+                LineNr = { fg = theme.syn.number, bold = true },
+                LineNrBelow = { fg = theme.ui.nontext },
+                Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
+                PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+                PmenuSbar = { bg = theme.ui.bg_m1 },
+                PmenuThumb = { bg = theme.ui.bg_p2 },
+                NormalFloat = { bg = "none" },
+                FloatBorder = { bg = "none" },
+                FloatTitle = { bg = "none" },
+                NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+                LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+                MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+                TelescopeTitle = { fg = theme.ui.special, bold = true },
+                TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+                TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+                TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+                TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+                TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+                TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+            }
+        end,
     },
-    overrides = function(colors)
-        local theme = colors.theme
-        return {
-            Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
-            PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
-            PmenuSbar = { bg = theme.ui.bg_m1 },
-            PmenuThumb = { bg = theme.ui.bg_p2 },
-            NormalFloat = { bg = "none" },
-            FloatBorder = { bg = "none" },
-            FloatTitle = { bg = "none" },
-            NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
-            LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-            MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-            TelescopeTitle = { fg = theme.ui.special, bold = true },
-            TelescopePromptNormal = { bg = theme.ui.bg_p1 },
-            TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
-            TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
-            TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
-            TelescopePreviewNormal = { bg = theme.ui.bg_dim },
-            TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
-        }
-    end,
     config = function(_, opts)
         require("kanagawa").setup(opts)
         vim.cmd.colorscheme("kanagawa")
